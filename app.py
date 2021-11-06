d459143f063309e63a402b69ce6bef0c158f171e
aa hurst <aahurst@aas-iMac.local>
2021-11-05 20:49:28 -0700

    work you fuckin nbastard

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, url_for, request
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return "<p>Hello, World!</p>"
+
+if __name__ == "__main__":
+    
+    app.run(debug = True)
