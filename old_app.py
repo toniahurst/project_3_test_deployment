044ff50b7b6cfe8524b67c7180ad5f2d3d317a59
Tonia Hurst <antonia.a.hurst@gmail.com>
2021-11-06 19:46:08 -0700

    updating app

--- old_app.py
+++ old_app.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def home():
     return "<p>Hello, NEW World!</p>"
 
-@app.route("/")
-def index():
-    return render_template("index.html")
-
 if __name__ == "__main__":
     
     app.run(debug = True)
