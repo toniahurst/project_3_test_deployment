1670f253698386ed87050d64dc6cb7a82e158cb2
Tonia Hurst <antonia.a.hurst@gmail.com>
2021-11-06 20:46:54 -0700

    updating app

--- app.py
+++ app.py
@@ -1,14 +1,4 @@
-from pathlib import Path
-import os
-import sys
-from bokeh.embed import components
 from flask import Flask, render_template, url_for, request
-import bokeh
-import pandas as pd
-## from util import make_plot
-import csv
-import json
-import hvplot.pandas
 
 app = Flask(__name__)
 
@@ -20,6 +10,7 @@ def home():
 if __name__ == "__main__":
     
     app.run(debug = True)
+    
 
 
     
