bda3f2764d664b17605567962d4914299b18745b
Tonia Hurst <antonia.a.hurst@gmail.com>
2021-11-06 20:38:57 -0700

    updating app

--- app.py
+++ app.py
@@ -1,15 +1,21 @@
+from pathlib import Path
+import os
+import sys
+from bokeh.embed import components
 from flask import Flask, render_template, url_for, request
+import bokeh
+import pandas as pd
+## from util import make_plot
+import csv
+import json
+import hvplot.pandas
 
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/home")
-def home():
-    return "<p>Hello, NEW World!</p>"
-
-@app.route("/")
+@app.route("/layout")
 def index():
-    return render_template("index.html")
+    return render_template("layout.html")
 
 if __name__ == "__main__":
     
